9ed76a926406820b348f159e0534b488d049c939
Sergei Chipiga <chipiga86@gmail.com>
2017-12-10 00:21:52 +0200

    Add unit test for lazy import

---
+++ tests/unit/testLazyImport.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var _ = require("lodash");
+var glaceImage = require("../../lib");
+
+
+test("Plugin lazy import", () => {
+
+    chunk("empty by default", () => {
+        expect(_.isEmpty(glaceImage)).to.be.true;
+    });
+
+    chunk("has config", () => {
+        expect(glaceImage.config).to.exist;
+    });
+
+    chunk("has plugin help", () => {
+        expect(glaceImage.pluginHelp).to.exist;
+    });
+
+    chunk("has steps", () => {
+        expect(glaceImage.Steps).to.exist;
+    });
+});
