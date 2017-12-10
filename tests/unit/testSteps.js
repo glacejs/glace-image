7eaf91d22881c44e9758276672568db0a829515c
Sergei Chipiga <chipiga86@gmail.com>
2017-12-10 12:02:54 +0200

    Add unit test for _displayScreenshot

--- tests/unit/testSteps.js
+++ tests/unit/testSteps.js
@@ -15,21 +15,38 @@ test(".makeScreenshot", () => {
         ctx._canvasScreenshot = sinon.spy();
         ctx._displayScreenshot = sinon.spy();
     });
-    chunk("should be taken via selenium by default", async () => {
+    chunk("is taken via selenium by default", async () => {
         await ctx.makeScreenshot({ check: false });
         expect(ctx._seleniumScreenshot.calledOnce).to.be.true;
     });
-    chunk("should be taken via selenium", async () => {
+    chunk("is taken via selenium", async () => {
         await ctx.makeScreenshot({ by: "selenium", check: false });
         expect(ctx._seleniumScreenshot.calledOnce).to.be.true;
     });
-    chunk("should be taken via html2canvas", async () => {
+    chunk("is taken via html2canvas", async () => {
         await ctx.makeScreenshot({ by: "html2canvas", check: false });
         expect(ctx._canvasScreenshot.calledOnce).to.be.true;
 
     });
-    chunk("should be taken via system display", async () => {
+    chunk("is taken via system display", async () => {
         await ctx.makeScreenshot({ by: "system", check: false });
         expect(ctx._displayScreenshot.calledOnce).to.be.true;
+    });
+});
+
+test("._displayScreenshot", () => {
+    var ctx = {};
+
+    before(() => {
+        ctx._displayScreenshot = Steps._displayScreenshot;
+    });
+
+    beforeChunk(() => {
+        ctx.__screenshot = sinon.spy((path, cb) => cb());
+    });
+
+    chunk("is passed", async () => {
+        await ctx._displayScreenshot("/image/path");
+        expect(ctx.__screenshot.calledOnce).to.be.true;
     });
 });
